101ff6c01985aee169eb69444a32d21ddca2dee2
Lokesh <gmlokesh@gmail.com>
2016-09-24 00:15:56 -0700

    check in to github

--- src/test/java/com/edu/misc/Temp.java
+++ src/test/java/com/edu/misc/Temp.java
@@ -1,143 +1,58 @@
 package com.edu.misc;
 
-import com.google.common.collect.Lists;
-import org.apache.commons.lang.math.RandomUtils;
-import org.junit.Assert;
 import org.junit.Test;
-
-import java.math.BigDecimal;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
+import java.util.Stack;
 
 public class Temp {
 
     @Test
-    public void verifySorting() {
-        final Employee firstEmployee = new Employee("Bob", "Finance");
-        final Employee secondEmployee = new Employee("Zyba", "Accounting");
-        final Employee thirdEmployee = new Employee("Alan", "IT");
+    public void verify() {
+        final Stack<String> stack = new Stack<>();
+        final String input = "1+2*4/5-7+3/6";
+        final String expected = "124*5/+7-36/+";
+        final String result = infixToPostfix(input);
+    }
 
-        final List<Employee> employees = Lists.newArrayList(firstEmployee, secondEmployee, thirdEmployee);
+    private String infixToPostfix(final String expression) {
+        String postFix = "";
+        Stack<Character> stack = new Stack<>();
 
-        Collections.sort(employees, new Comparator<Employee>() {
-
-            public int compare(final Employee o1, final Employee o2) {
-                return o1.getName().compareToIgnoreCase(o2.getName());
+        for (int i = 0; i < expression.length(); i++) {
+            if (isOperator(expression.charAt(i))) {
             }
-        });
-
-        for (final Employee employee : employees) {
-            System.out.println(employee.getName());
+            System.out.println(isOperand(expression.charAt(i)));
+            hasHigherPrecedence(stack.peek(), expression.charAt(i));
         }
+        return postFix;
     }
 
-    class Employee {
-        String name;
-        String dept;
-
-        Employee(final String name, final String dept) {
-            this.name = name;
-            this.dept = dept;
-        }
-
-        public String getName() {
-            return name;
-        }
+    private boolean isOperator(final char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '%';
     }
 
-    @Test
-    public void checkConditionalFlow() {
-        final List<String> testStringList;
+    private boolean isOperand(final char c) {
+        return c >= '0' && c <= '9' || c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
+    }
 
-        if (isValid()) {
-            testStringList = Lists.newArrayList("testTrue");
-        } else {
-            testStringList = Lists.newArrayList("testFalse");
+    private boolean hasHigherPrecedence(final char c1, final char c2) {
+        return getOperatorWeight(c1) > getOperatorWeight(c2);
+    }
+
+    private int getOperatorWeight(final char c) {
+
+        int weight = -1;
+
+        switch (c) {
+            case '+':
+            case '-':
+                weight = 1;
+                break;
+            case '*':
+            case '/':
+            case '%':
+                weight = 2;
         }
-
-        for (String str : testStringList) {
-            System.out.println(str);
-        }
-    }
-    private boolean isValid() {
-        return true;
-    }
-
-    @Test
-    public void check() {
-        final Data dataOne = new Data(RandomUtils.nextInt(), RandomUtils.nextDouble() * 1000);
-        final Data dataTwo = new Data(RandomUtils.nextInt(), RandomUtils.nextDouble() * 1000);
-
-        List<Data> dataList = Lists.newArrayList(dataOne, dataTwo);
-    }
-
-    @Test
-    public void nullCheck1() {
-        List<Data> dataList = Lists.newArrayList();
-        final Data data = new Data();
-        data.setId(RandomUtils.nextInt());
-        dataList.add(data);
-        final BigDecimal perc = BigDecimal.valueOf(dataList.get(0).getPerc());
-        System.out.println(perc);
-
-        System.out.println(BigDecimal.ZERO);
-    }
-
-    @Test
-    public void nullCheck2() {
-        List<Data> dataList = Lists.newArrayList();
-        final double perc = dataList.get(0).getPerc();
-        System.out.println(perc);
-    }
-
-    @Test
-    public void checkBigDecimal() {
-        final BigDecimal one = BigDecimal.valueOf(RandomUtils.nextDouble() * 1000);
-        System.out.println(one);
-    }
-
-    class Data {
-        int id;
-        double perc;
-
-        Data() {
-        }
-
-        Data(final int id, final double perc) {
-            this.id = id;
-            this.perc = perc;
-        }
-
-        public double getPerc() {
-            return perc;
-        }
-
-        public void setId(final int id) {
-            this.id = id;
-        }
-    }
-
-    @Test
-    public void checkDelta() {
-        final double expected = 3249.12;
-        final double actual = 3249.13;
-        final double delta = 0.02;
-
-        if (expected - actual < delta) {
-            System.out.println("true");
-        } else {
-            System.out.println("false");
-        }
-        Assert.assertEquals(expected, actual, delta);
-    }
-
-    @Test
-    public void checkStringFormat() {
-        Date date = new Date();
-        String result = String.format("Date is %tD", date);
-        System.out.println(result);
+        return weight;
     }
 
 }
