a5c951d080e1275186ca02cc762edb0208b6865a
Lokesh <gmlokesh@gmail.com>
2017-02-20 20:05:02 -0800

    patterns practice...

---
+++ src/main/java/com/edu/misc/MyServlet.java
@@ -0,0 +1,15 @@
+package com.edu.misc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, java.io.IOException {
+
+    }
+
+}
